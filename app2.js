a78e73ee4edaaacd27d561c0d2188e56e0b82d56
magiarus <rus.rajapov@gmail.com>
2023-02-10 14:57:10 +0600

    first commit

---
+++ app2.js
@@ -0,0 +1,12 @@
+const getUsers = async () => {
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await users.json();
+    for (key in data) {
+      document.getElementById("ulist").innerHTML += `
+      <li>Name: ${data[key].name}; Phone:  ${data[key].phone}</li>
+      `;
+    }
+    //   console.log(data[9].name);
+    return data;
+  };
+  getUsers();
